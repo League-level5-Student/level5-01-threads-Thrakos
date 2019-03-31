ef21d3771189ab7bc11f20bebbc721c0c71b936d
Thrakos <league@iMac-7.attlocal.net>
2019-03-31 16:53:00 -0700

    threadPool, synchronizedSwimming

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -5,15 +5,35 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class ThreadPool {
 
 	private Thread[] thread;
-	private ConcurrentLinkedQueue taskQueue;
+	private ConcurrentLinkedQueue<Task> taskQueue;
 	
 	public ThreadPool(int totalThreads) {
 		
+		taskQueue = new ConcurrentLinkedQueue<Task>();
+
 		thread = new Thread[totalThreads];
 		for (int i = 0; i < thread.length; i++) {
-			
+			thread[i] = new Thread(new Worker(taskQueue));
 		}
 		
+	}
+	
+	public void addTask(Task task) {
+		taskQueue.add(task);
+	}
+	
+	public void start() {
+		for (int i = 0; i < thread.length; i++) {
+			thread[i].start();
+		}
+		
+		for (int i = 0; i < thread.length; i++) {
+			try {
+				thread[i].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 }
--- src/_04_Thread_Pool/Worker.java
+++ src/_04_Thread_Pool/Worker.java
@@ -1,12 +1,22 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Worker implements Runnable {
 
+	private ConcurrentLinkedQueue<Task> taskQueue;
+	
+	public Worker(ConcurrentLinkedQueue<Task> taskQueue) {
+		
+		this.taskQueue = taskQueue;
+		
+	}
+	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		while(!taskQueue.isEmpty()) {
+			taskQueue.remove().perform();
+		}
 	}
 
 
